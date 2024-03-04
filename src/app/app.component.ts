cfe5f7804170eb66d12cb539202c012834dabf11
SaiPedarla <saiphanindrap1993@gmail.com>
2024-03-03 23:46:02 -0600

    Add existing project files prior to the push to GitHub.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root', // Adjust if needed
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  // Add this line
+  standalone: true
 })
 export class AppComponent {
   title = 'angular-products-app';
